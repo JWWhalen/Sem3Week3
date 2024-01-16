c147a284af50eb3f29b59d76503ef060dbbd8f0b
Justin Whalen <138682790+JWWhalen@users.noreply.github.com>
2024-01-16 14:25:47 -0330

    Added http and more Validator examples

--- http.js
+++ http.js
@@ -1,6 +1,7 @@
 var http = require('http');
 
-serveText('Hello World');
+//serveText('Hello World');
+serveHtml('<body> <h1>Hello World from HTML</h1> <p>This is the HTML part of the response</p></body>');
 
 function serveText(theText) {
     http.createServer(function(req, res) {
@@ -8,6 +9,16 @@ function serveText(theText) {
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.write(theText);
         res.write(', eat more by')
+        res.end();
+    }).listen(8080);
+}
+
+function serveHtml(theHtml) {
+    http.createServer(function(req, res) {
+        console.log('html was served.')
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(theHtml);
+        res.write('This is the HTML part of the ')
         res.end();
     }).listen(8080);
 }
